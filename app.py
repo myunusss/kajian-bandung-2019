e8f4d4ff81a02b11280bdbd90d7028de03e5e77e
muhammad yunus <turmudi.yunus@gmail.com>
2019-09-17 22:19:06 +0700

    coba

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from resources.todo import Todo
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Todo, "/todo/<int:id>")
+# api.add_resource(Todo, "/todo/<int:id>")
+api.add_resource(Todo, "/todo")
 
 if __name__ == "__main__":
   app.run()
--- resources/todo.py
+++ resources/todo.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from dbconnect import ConnectDB, CloseDB
+from common.app_setting import responseCode, responseList, responseText, detail
 
 todos = [
   {
@@ -20,17 +21,28 @@ todos = [
 ]
 
 class Todo(Resource):
-  def get(self, id):
-    conn, cur = ConnectDB()
-    hasil = ""
+  def get(self):
+    conn, cur = ConnectDB()    
     try:
       cur.execute("select id_kajian, tanggal, deskripsi from kajian")
-      hasil = "Berhasil"
+      data = []
+      for row in cur:
+        v_id = row[0]
+        v_tanggal = row[1]
+        v_deskripsi = row[2]
+
+        data.append({
+            str("id"):str(v_id),
+            str("tanggal"):str(v_tanggal),
+            str("deskripsi"):str(v_deskripsi)
+        })
+
+      result = {responseCode:"200", responseText:"success", responseList:data}
     except Exception as e:
-      hasil = "Oopss"
+      result = {responseCode:"404", responseText:"Not found", detail:str(e)}
     finally:
       CloseDB(conn, cur)
-    return hasil
+    return result
     # for todo in todos:
     #   if(id == todo["id"]):
     #     return todo, 200

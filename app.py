2901fe67745c80ac94e3a5b554851f36af6950cb
muhammad yunus <turmudi.yunus@gmail.com>
2019-09-17 22:11:29 +0700

    coba

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from flask.ext.sqlalchemy import SQLAlchemy
 
 from resources.todo import Todo
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
 api = Api(app)
 
 api.add_resource(Todo, "/todo/<int:id>")
--- dbconnect.py
+++ dbconnect.py
@@ -1,18 +1,20 @@
 from flask import Flask
 import psycopg2
-import os
-
-DATABASE_URL = os.environ['postgres://qugjbrgnwcorxs:51ba6fbd895d056e4446b56d23a539673b496ffe78ca3fe95ea00315d1e7e32b@ec2-54-221-215-228.compute-1.amazonaws.com:5432/db1ee1t553hrtj']
 
 def ConnectDB():
+    connectionString = 'dbname=db1ee1t553hrtj host=ec2-54-221-215-228.compute-1.amazonaws.com user=qugjbrgnwcorxs password=51ba6fbd895d056e4446b56d23a539673b496ffe78ca3fe95ea00315d1e7e32b port=5432'
+    
     try:
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        conn = psycopg2.connect(connectionString, sslmode='require')
         cur = conn.cursor()
 
+        conn.autocommit=False
+        
         return conn, cur
     except:
         print("Error executing select")
 
 def CloseDB(conn, cur):
+    
     conn.close()
     cur.close()

9b2bdeead9978b9a30757630854d490c8c1f31b6
muhammad yunus <turmudi.yunus@gmail.com>
2019-09-18 12:45:48 +0700

    penambahan query

--- resources/kajian.py
+++ resources/kajian.py
@@ -6,17 +6,25 @@ class Kajian(Resource):
   def get(self):
     conn, cur = ConnectDB()    
     try:
-        cur.execute("select id_kajian, tanggal, deskripsi from kajian")
+        cur.execute("select id_kajian, tanggal, deskripsi, nama_pemateri, poster_path " +
+        "inner join kajian_pemateri using (id_kajian) " +
+        "inner join kajian_poster using (id_kajian) " +
+        "inner join pemateri using (id_pemateri) " +
+        "from kajian")
         data = []
         for row in cur:
             v_id = row[0]
             v_tanggal = row[1]
             v_deskripsi = row[2]
+            v_pemateri = row[3]
+            v_poster_path = row[4]
 
             data.append({
                 str("id"):str(v_id),
                 str("tanggal"):str(v_tanggal),
-                str("deskripsi"):str(v_deskripsi)
+                str("deskripsi"):str(v_deskripsi),
+                str("pemateri"):str(v_pemateri),
+                str("poster_path"):str(v_poster_path)
             })
 
         result = {responseCode:"200", responseText:"success", responseList:data}

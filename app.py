2b3e1c6556de317dc3fb987ba9d525c9903749e9
muhammad yunus <turmudi.yunus@gmail.com>
2019-09-19 14:14:02 +0700

    list kajian

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ api = Api(app)
 # api.add_resource(Todo, "/todo/<int:id>")
 api.add_resource(Kajian, "/kajian")
 api.add_resource(Kolaborasi, "/tim/kolaborasi")
-api.add_resource(Kajian, "/list/kajian")
+api.add_resource(ListKajian, "/list/kajian")
 
 if __name__ == "__main__":
   app.run()

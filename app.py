861edcec1e7d5c2733a9496848457d51b295d789
muhammad yunus <turmudi.yunus@gmail.com>
2019-09-22 09:56:42 +0700

    Menambahkan iklan

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 
 from resources.kajian import Kajian, ListKajian
 from resources.kolaborasi import Kolaborasi
+from resources.iklan import Iklan
+from resources.quote import Quote
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,8 +13,8 @@ api = Api(app)
 api.add_resource(Kajian, "/kajian")
 api.add_resource(ListKajian, "/list/kajian")
 api.add_resource(Kolaborasi, "/tim/kolaborasi")
-api.add_resource(Kajian, "/quote")
-api.add_resource(Kajian, "/iklan")
+api.add_resource(Quote, "/quote")
+api.add_resource(Iklan, "/iklan")
 
 if __name__ == "__main__":
   app.run()

66c27c0788cd98b1fc0dd94faa45ff9a26d6d9ee
muhammad yunus <turmudi.yunus@gmail.com>
2019-09-24 22:49:53 +0700

    kembali

--- resources/kajian.py
+++ resources/kajian.py
@@ -5,52 +5,52 @@ from datetime import datetime
 from common.app_setting import responseCode, responseList, responseText, detail, _id, tanggal, deskripsi, pemateri, poster_path, bulan, hari, full_date, tempat, judul
 
 class Kajian(Resource):
-    def post(self):
-        if (request.form.get("date") != None):
-            date = request.form.get("date")
+  def post(self):
+    if (request.form.get("date") != None):
+        date = request.form.get("date")
+    else:
+        date = ""
+
+    if (request.form.get("session_token") != None):
+        session_token = request.form.get("session_token")
+    else:
+        session_token = ""
+
+    conn, cur = ConnectDB()
+    try:
+        if (session_token == '$2y$12$/Am4ByLydvLE4ra2pvGDUOkDWYRi5XObtfqH/SWpRJAnJY8/dzDsS'):
+            cur.execute("select id_kajian, tanggal, deskripsi, nama_pemateri, poster_path " +
+            "from kajian " +
+            "inner join kajian_pemateri using (id_kajian) " +
+            "inner join kajian_poster using (id_kajian) " +
+            "inner join pemateri using (id_pemateri) " +
+            "where date(tanggal) = %s", [date])
+            
+            data = []
+
+            for row in cur:
+                v_id = row[0]
+                v_tanggal = row[1]
+                v_deskripsi = row[2]
+                v_pemateri = row[3]
+                v_poster_path = row[4]
+
+                data.append({
+                    _id:str(v_id),
+                    tanggal:str(v_tanggal),
+                    deskripsi:str(v_deskripsi),
+                    pemateri:str(v_pemateri),
+                    poster_path:str(v_poster_path)
+                })
+
+            result = {responseCode:"200", responseText:"success", responseList:data}
         else:
-            date = ""
-
-        if (request.form.get("session_token") != None):
-            session_token = request.form.get("session_token")
-        else:
-            session_token = ""
-
-        conn, cur = ConnectDB()
-        try:
-            if (session_token == '$2y$12$/Am4ByLydvLE4ra2pvGDUOkDWYRi5XObtfqH/SWpRJAnJY8/dzDsS'):
-                cur.execute("select id_kajian, tanggal, deskripsi, nama_pemateri, poster_path " +
-                "from kajian " +
-                "inner join kajian_pemateri using (id_kajian) " +
-                "inner join kajian_poster using (id_kajian) " +
-                "inner join pemateri using (id_pemateri) " +
-                "where date(tanggal) = %s", [date])
-                
-                data = []
-
-                for row in cur:
-                    v_id = row[0]
-                    v_tanggal = row[1]
-                    v_deskripsi = row[2]
-                    v_pemateri = row[3]
-                    v_poster_path = row[4]
-
-                    data.append({
-                        _id:str(v_id),
-                        tanggal:str(v_tanggal),
-                        deskripsi:str(v_deskripsi),
-                        pemateri:str(v_pemateri),
-                        poster_path:str(v_poster_path)
-                    })
-
-                result = {responseCode:"200", responseText:"success", responseList:data}
-            else:
-                result = {responseCode:"401", responseText:"Ooppss..."}
-        except Exception as e:
-            result = {responseCode:"404", responseText:"Not found", detail:str(e)}
-        finally:
-            CloseDB(conn, cur)
-        return result
+            result = {responseCode:"401", responseText:"Ooppss..."}
+    except Exception as e:
+        result = {responseCode:"404", responseText:"Not found", detail:str(e)}
+    finally:
+        CloseDB(conn, cur)
+    return result
 
 # class DetailKajian(Resource):
 #   def post(self):
@@ -96,14 +96,52 @@ class Kajian(Resource):
 #         CloseDB(conn, cur)
 #     return result
 
-class DetailKajian(Resource):
-    def get(self):
-        result = {responseCode:"404", responseText:"Not found", detail:str(e)}
-        
-        return result
+# class DetailKajian(Resource):
+#   def post(self):
+#     if (request.form.get("id_kajian") != None):
+#         id_kajian = request.form.get("id_kajian")
+#     else:
+#         id_kajian = ""
+
+#     if (request.form.get("session_token") != None):
+#         session_token = request.form.get("session_token")
+#     else:
+#         session_token = ""
+
+#     conn, cur = ConnectDB()
+#     try:
+#         if (session_token == '$2y$12$/Am4ByLydvLE4ra2pvGDUOkDWYRi5XObtfqH/SWpRJAnJY8/dzDsS'):
+#             cur.execute("select id_kajian, tanggal, deskripsi, nama_pemateri, poster_path, tempat, judul, p.panggilan " +
+#             "from kajian " +
+#             "inner join kajian_pemateri using (id_kajian) " +
+#             "inner join kajian_poster using (id_kajian) " +
+#             "inner join pemateri p using (id_pemateri) " +
+#             "where id_kajian = %s", [id_kajian])
+            
+#             row = cur.fetchone()
+
+#             if (row !== None):
+#                 v_id = row[0]
+#                 v_tanggal = row[1]
+#                 v_deskripsi = row[2]
+#                 v_poster_path = row[4]
+#                 v_tempat = row[5]
+#                 v_judul = row[6]
+#                 v_pemateri = row[7] + ' ' + row[3]
+
+#                 result = {responseCode:"200", responseText:"success", _id:str(v_id), tanggal:str(v_tanggal), deskripsi:v_deskripsi, poster_path:v_poster_path, tempat:v_tempat, judul:v_judul, pemateri:str(v_pemateri)}
+#             else:
+#                 result = {responseCode:"401", responseText:"Not found"}
+#         else:
+#             result = {responseCode:"401", responseText:"Ooppss..."}
+#     except Exception as e:
+#         result = {responseCode:"404", responseText:"Not found", detail:str(e)}
+#     finally:
+#         CloseDB(conn, cur)
+#     return result
 
 class ListKajian(Resource):
-    def post(self):
+  def post(self):
     if (request.form.get("session_token") != None):
         session_token = request.form.get("session_token")
     else:
